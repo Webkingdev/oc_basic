1fc283fe67196fb9b2f8cd21e9ec56d57412ca5f
NadiiaKov <webking.dev3@gmail.com>
2016-11-10 17:49:00 +0200

    Папка назначения для страницы категорий - "shop"

---
+++ catalog/controller/shop/shop.php
@@ -0,0 +1,87 @@
+<?php
+class ControllerShopShop extends Controller {
+	public function index() {
+		$this->load->language('extension/module/category');
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$this->document->setTitle($this->config->get('config_meta_title'));
+		$this->document->setDescription($this->config->get('config_meta_description'));
+		$this->document->setKeywords($this->config->get('config_meta_keyword'));
+
+		if (isset($this->request->get['route'])) {
+			$this->document->addLink($this->config->get('config_url'), 'canonical');
+		}
+
+		if (isset($this->request->get['path'])) {
+			$parts = explode('_', (string)$this->request->get['path']);
+		} else {
+			$parts = array();
+		}
+
+		if (isset($parts[0])) {
+			$data['category_id'] = $parts[0];
+		} else {
+			$data['category_id'] = 0;
+		}
+
+		if (isset($parts[1])) {
+			$data['child_id'] = $parts[1];
+		} else {
+			$data['child_id'] = 0;
+		}
+
+		$this->load->model('catalog/category');
+		$this->load->model('catalog/product');
+		$this->load->model('tool/image');
+
+		$data['all_categories'] = array();
+		$allcategories = $this->model_catalog_category->getCategories();
+
+		foreach ($allcategories as $category) {
+			$children_data = array();
+
+			if ($category['category_id'] == $data['category_id']) {
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+
+				foreach($children as $child) {
+					$filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
+
+					$children_data[] = array(
+						'category_id' => $child['category_id'],
+						'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+					);
+				}
+			}
+
+			if ($category['image'] !== ''){
+				$image_src = $this->model_tool_image->resize($category['image'], $this->config->get($this->config->get('config_theme').'_image_category_width'), $this->config->get($this->config->get('config_theme').'_image_category_height'));
+			}else{
+				$image_src = 'image/catalog_200x200.png';
+			}
+			
+			$filter_data = array(
+				'filter_category_id'  => $category['category_id'],
+				'filter_sub_category' => true
+			);
+
+			$data['all_categories'][] = array(
+				'category_id' => $category['category_id'],
+				'name'		  => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+				'children'	  => $children_data,
+				'href'		  => $this->url->link('product/category', 'path=' . $category['category_id']),
+				'image'		  => $image_src
+			);
+		}
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('shop/shop', $data));
+	}
+}
---
+++ catalog/view/theme/default/template/shop/shop.tpl
@@ -0,0 +1,32 @@
+<?php echo $header; ?>
+<div class="container categories_page">
+    <?php echo $content_top; ?>
+    <div class="row"><?php echo $column_left; ?>
+        <?php if ($column_left && $column_right) { ?>
+        <?php $class = 'col-sm-6'; ?>
+        <?php } elseif ($column_left || $column_right) { ?>
+        <?php $class = 'col-sm-9'; ?>
+        <?php } else { ?>
+        <?php $class = 'col-sm-12'; ?>
+        <?php } ?>
+        <div id="content" class="<?php echo $class; ?>">
+            <ul>
+            <?php foreach ($all_categories as $category) { ?>
+                <li class="col-lg-3 col-md-4 col-sm-4 col-xs-6">
+                    <a href="<?php echo $category['href']; ?>">
+                        <span class="img_wrap">
+                            <img src="<?php echo $category['image']?>" alt="">
+                        </span><br>
+                        <span class="title_wrap">
+                            <?php echo $category['name']; ?>
+                        </span>
+                    </a>
+                </li>
+            <?php } ?>
+            </ul>
+        </div>
+        <?php echo $column_right; ?>
+    </div>
+    <?php echo $content_bottom; ?>
+</div>
+<?php echo $footer; ?>
